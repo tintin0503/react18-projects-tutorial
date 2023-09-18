8cb17f15b009a927fed00ec64858ac5263f8faaf
tintin0503 <quanptit2801@gmail.com>
2023-09-18 11:27:06 +0700

    init slice

---
+++ 18-redux-toolkit/src/features/cart/cartSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit'
+import cartItems from '../../cartItems'
+
+const initialState = {
+  cartItems: cartItems,
+  amount: 5,
+  total: 10,
+  isLoading: true,
+}
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+})
+
+console.log(cartSlice)
+
+export default cartSlice.reducer
