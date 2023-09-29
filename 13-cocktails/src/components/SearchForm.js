a2cd5b3ffa617c72837f302aaa3db9db1d13974b
quanbv <quanbv@reactplus.jp>
2023-09-29 16:14:54 +0700

    update

--- 13-cocktails/src/components/SearchForm.js
+++ 13-cocktails/src/components/SearchForm.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useGlobalContext } from '../context'
 
 const SearchForm = () => {
+  const { setSearchTerm } = useGlobalContext()
+  const searchValue = useRef('')
+
+  useEffect(() => {
+    searchValue.current.focus()
+  }, [])
+
+  const searchCocktail = () => {
+    console.log('searchValue.current.value ', searchValue.current.value)
+    setSearchTerm(searchValue.current.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className="section search">
+      <form className="search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name"> search your favorite cocktail</label>
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
   )
 }
 
--- 13-cocktails/src/pages/About.js
+++ 13-cocktails/src/pages/About.js
@@ -2,9 +2,26 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Esse commodo esse in consectetur est exercitation veniam reprehenderit
+        non dolore cillum cillum mollit. Qui nulla excepteur magna in sint eu
+        nostrud consectetur ex qui ad esse aliquip aliqua. Enim consequat magna
+        sunt aliquip dolore ullamco ea esse Lorem. Fugiat incididunt nisi cillum
+        tempor nisi ad mollit tempor exercitation. Cupidatat sint cillum aliquip
+        proident voluptate eu nostrud laborum mollit esse id occaecat magna
+        labore. In aute sunt est aliqua dolore commodo. Anim in sunt nisi ad
+        adipisicing. Mollit id elit magna exercitation sunt cillum laborum Lorem
+        in adipisicing velit nisi. Sint velit labore enim enim magna. Nulla eu
+        nisi sunt in irure ipsum minim reprehenderit dolore laborum qui
+        adipisicing duis sunt. Aliqua est ipsum exercitation ullamco non sint
+        irure adipisicing non mollit veniam labore adipisicing. Officia
+        consectetur ex velit consectetur laboris ea dolor dolore fugiat aliquip.
+        Elit cillum tempor fugiat cillum. Ullamco consectetur esse sint dolor
+        officia.
+      </p>
+    </section>
   )
 }
 
